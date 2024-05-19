efd64463072e10fae554a14fcda5777896fd24fd
Ashish-Gupta7 <ash300mr@gmail.com>
2024-05-19 07:30:34 +0530

    operators

---
+++ operators.java
@@ -0,0 +1,85 @@
+// Arithmetic operations
+// public class operators {
+//     public static void main(String args[]) {
+//         int a = 10;
+//         int b = 5;
+//         System.out.println("sum: " + (a + b));
+//         System.out.println("minus: " + (a - b));
+//         System.out.println("multiply: " + (a * b));
+//         System.out.println("divide: " + (a / b));
+//         System.out.println("modulo: " + (a % b));
+
+        // unary operations
+        // int c = a++;
+        // System.out.println(c);
+        // System.out.println(a);
+        // int c = ++a;
+        // System.out.println(c);
+        // System.out.println(a);
+        // int c = a--;
+        // System.out.println(c);
+        // System.out.println(a);
+        // int c = --a;
+        // System.out.println(c);
+        // System.out.println(a);
+//     }
+// }
+
+// Relational operators
+// public class operators {
+//     public static void main(String args[]) {
+//         int a = 10;
+//         int b = 5;
+//         int c = 10; 
+//         System.out.println((a == b));
+//         System.out.println((a == c));
+//         System.out.println((a != c));
+//         System.out.println((a != b));
+//         System.out.println((a > b));
+//         System.out.println((a > c));
+//         System.out.println((a < c));
+//         System.out.println((a < b));
+//         System.out.println((a <= b));
+//         System.out.println((a <= c));
+//         System.out.println((c <= b));
+//         System.out.println((a >= b));
+//         System.out.println((a >= c));
+//         System.out.println((c >= b));
+//     }
+// }
+
+
+// Logical operators
+// public class operators {
+//     public static void main(String args[]) {
+//         System.out.println("a: " + (3>2 && 2>1));
+//         System.out.println("b: " + (3>2 && 2<1));
+//         System.out.println("c: " + (3<2 && 2>1));
+//         System.out.println("d: " + (3<2 && 2<1));
+
+//         System.out.println("a: " + (3>2 || 2>1));
+//         System.out.println("b: " + (3>2 || 2<1));
+//         System.out.println("c: " + (3<2 || 2>1));
+//         System.out.println("d: " + (3<2 || 2<1));
+
+//         System.out.println("a: " + !(3<2 || 2<1));
+//         System.out.println("b: " + !(3>2 || 2<1));
+
+
+//     }
+// }
+
+
+// Assignment operators
+// public class operators {
+//     public static void main(String args[]) {
+//         int a = 10;
+//         int b = a;
+//         int c = 5;
+//         System.out.println(a);
+//         System.out.println(a += 10);
+//         System.out.println(a -= 10);
+//         System.out.println(a += c);
+//         System.out.println(a *= c);
+//     }
+// }
