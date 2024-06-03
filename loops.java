b8aed96069e115fcf834cd293ba220d20392abcf
Ashish-Gupta7 <ash300mr@gmail.com>
2024-06-03 12:44:28 +0530

    loops que

--- loops.java
+++ loops.java
@@ -231,3 +231,60 @@
 //         }
 //     }
 // }
+
+// Write a program that reads a set of integers, and then prints the sum of the even and odd integers.
+// import java.util.Scanner;
+// public class loops {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int number;
+//         int choice;
+//         int evenSum = 0;
+//         int oddSum = 0;
+
+//         do {
+//             number = sc.nextInt();
+//             if(number % 2 == 0) {
+//                 evenSum += number;
+//             } else{
+//                 oddSum += number;
+//             }
+
+//             System.out.println("Do you want to countinue? Press 1 for yes or 0 for no.");
+//             choice = sc.nextInt();
+//         } while(choice == 1);
+
+//         System.out.println("Sum of even numbers : " + evenSum);
+//         System.out.println("Sum of odd numbers : " + oddSum);
+//     }
+// }
+
+// Write a program to find the factorial of any number entered by the user.
+// import java.util.Scanner;
+// public class loops {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int factorial = 1;
+//         int number = sc.nextInt();
+
+//         for(int i = 1; i <= number; i++) {
+//             factorial *= i;
+//         System.out.println("Factorial : " + factorial);
+//         }
+//         System.out.println("Factorial : " + factorial);
+//     }
+// }
+
+// Write a program to print the multiplication table of a number N, entered by the user.
+// import java.util.Scanner;
+// public class loops {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int number = sc.nextInt();
+//         System.out.println("the multiplication table of : " + number);
+//         for(int i = 1; i <= 10; i++) {
+//             System.out.println(number + " * " + i + " = " + number*i);
+//         }
+//     }
+// }
