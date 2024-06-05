7844c9cbec2dda144de429deb8909b3d09102083
Ashish-Gupta7 <ash300mr@gmail.com>
2024-06-05 11:55:51 +0530

    fnc and method

---
+++ FncAndMethods.java
@@ -0,0 +1,46 @@
+// print hello
+// public class FncAndMethods {
+
+//     public static void printHello() {
+//         System.out.println("Hello");
+//         System.out.println("Hello");
+//         System.out.println("Hello");
+//     }
+
+//     public static void main(String args[]) {
+//         printHello();
+//     }
+// }
+
+// calculate sum using function parameter - return type void
+// import java.util.Scanner;
+// public class FncAndMethods {
+
+//     public static void calculateSum(int a, int b) {
+//         int sum = a + b;
+//         System.out.println("sum is : " + sum);
+//     }
+
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int num1 = sc.nextInt();
+//         int num2 = sc.nextInt();
+//         calculateSum(num1, num2);
+//     }
+// }
+
+// calculate sum using function parameter - return type int
+// import java.util.Scanner;
+// public class FncAndMethods {
+//     public static int calculateSum(int a, int b) {
+//         int sum = a + b;
+//         return sum;
+//     } 
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int num1 = sc.nextInt();
+//         int num2 = sc.nextInt();
+//         int sum = calculateSum(num1, num2);
+//         System.out.println(sum);
+//     }
+// }
