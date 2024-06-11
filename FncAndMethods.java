e6bc1df30b931f3868e004dc8b7e33e433678d07
Ashish-Gupta7 <ash300mr@gmail.com>
2024-06-11 16:53:29 +0530

    multiplication

--- FncAndMethods.java
+++ FncAndMethods.java
@@ -44,3 +44,17 @@
 //         System.out.println(sum);
 //     }
 // }
+
+// calculate multiplication of two numbers - return type int
+// public class FncAndMethods {
+//     public static int multiply(int a, int b) {
+//         int product = a * b;
+//         return product;
+//     }
+//     public static void main(String args[]) {
+//         int a = 3;
+//         int b = 5;
+//         int prod = multiply(a, b);
+//         System.out.println(prod);
+//     }
+// }
